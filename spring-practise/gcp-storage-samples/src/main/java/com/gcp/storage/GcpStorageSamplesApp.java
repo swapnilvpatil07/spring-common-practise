f18c94910291cefbb73b93f56234761dbde0690c
Swapnil Patil <swapnilvpatil07@gmail.com>
2023-01-21 22:54:31 +0530

    added sub-module for gcp storage samples

---
+++ spring-practise/gcp-storage-samples/src/main/java/com/gcp/storage/GcpStorageSamplesApp.java
@@ -0,0 +1,18 @@
+package com.gcp.storage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ */
+
+/**
+ * @author Nilam Patil
+ *
+ */
+@SpringBootApplication
+public class GcpStorageSamplesApp {
+	public static void main(String[] args) {
+		SpringApplication.run(GcpStorageSamplesApp.class, args);
+	}
+}
