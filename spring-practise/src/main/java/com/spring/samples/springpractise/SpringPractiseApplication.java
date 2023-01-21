8806954d5e5b21ceb47f9aa1a765633f55c0a8d0
Swapnil Patil <swapnilvpatil07@gmail.com>
2023-01-21 21:26:18 +0530

    initial commit

---
+++ spring-practise/src/main/java/com/spring/samples/springpractise/SpringPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.spring.samples.springpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractiseApplication.class, args);
+	}
+
+}
---
+++ spring-practise/src/test/java/com/spring/samples/springpractise/SpringPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.samples.springpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
